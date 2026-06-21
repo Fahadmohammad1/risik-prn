30cf9c440800aebae2c1a54b2c64d201321b1a78
MohammadRijuan <mohammadrijuan2529@gmail.com>
2026-06-21 21:39:42 +0600

    done responsive

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -8,10 +8,10 @@ export default function AuthLayout({
     children: React.ReactNode
 }) {
     return (
-        <div className="max-w-[1392px]  mx-auto p-6 flex gap-[48px] items-start">
+        <div className="max-w-[1392px] mx-auto p-4 sm:p-6 flex flex-col lg:flex-row gap-6 lg:gap-[48px] items-start">
 
-            {/* LEFT IMAGE (shared) */}
-            <div className="w-[740px] h-[852px]">
+            {/* LEFT IMAGE */}
+            <div className="w-full lg:w-[740px] h-[300px] sm:h-[450px] lg:h-[852px]">
                 <Image
                     src={logo}
                     alt="Auth Image"
@@ -19,19 +19,19 @@ export default function AuthLayout({
                 />
             </div>
 
-            {/* RIGHT SIDE (dynamic form) */}
-            <div className="relative flex gap-[46px] flex-col w-[604px] pt-[32px]">
+            {/* RIGHT SIDE */}
+            <div className="relative flex flex-col w-full lg:w-[604px] pt-6 lg:pt-[32px]">
 
                 {/* CONTENT */}
-                <div className="relative z-10">
+                <div className="relative z-10 w-full">
                     {children}
                 </div>
 
-                {/* BACKGROUND VECTOR IMAGE */}
+                {/* BACKGROUND VECTOR */}
                 <Image
                     src={vector1}
                     alt="Auth Image"
-                    className="absolute -bottom-[290px] right-0 w-[659px] h-auto object-contain rounded-[20px] -z-10"
+                    className="hidden lg:block absolute -bottom-[290px] right-0 w-[659px] h-auto object-contain rounded-[20px] -z-10"
                 />
 
             </div>
